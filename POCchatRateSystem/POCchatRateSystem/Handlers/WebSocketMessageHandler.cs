849c2ee3704ae0be7f25a8faffaed898f0c41557
Hamza <hamza.gaizi@hotmail.com>
2021-04-20 17:54:51 +0200

    Projectbug

---
+++ POCchatRateSystem/POCchatRateSystem/Handlers/WebSocketMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using POCchatRateSystem.SocketsManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCchatRateSystem.Handlers
+{
+    public class WebSocketMessageHandler:SocketHandler
+    {
+        public WebSocketMessageHandler(ConnectionManager connections): base(connections)
+        {
+
+
+        }
+        public override async Task OnConnected(WebSocket socket)
+        {
+            await base.OnConnected(socket);
+            var socketId = Connections.GetID(socket);
+            await SendMessageToAll($"{socketId} just joined the party *****");
+
+        }
+        public override async Task Receive(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            var socketId = Connections.GetID(socket);
+            var message = $"{socketId} said : {Encoding.UTF8.GetString(buffer, 0, result.Count)}";
+            await SendMessageToAll(message);
+        }
+    }
+}
--- POCchatRateSystem/POCchatRateSystem/Startup.cs
+++ POCchatRateSystem/POCchatRateSystem/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using POCchatRateSystem.Handlers;
+using POCchatRateSystem.SocketsManager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,34 +28,21 @@ namespace POCchatRateSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "POCchatRateSystem", Version = "v1" });
-            });
+            services.AddWebSocketManager();
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "POCchatRateSystem v1"));
+              
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseWebSockets();
+            app.MapSockets("/ws",serviceProvider.GetService<WebSocketMessageHandler>());
+            app.UseStaticFiles();
         }
     }
 }
